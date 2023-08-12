276718e1d7e9c0bcb2326a55a146a4b257128e58
Alessandro Abati <alessandro.abati3@gmail.com>
2023-08-12 10:43:42 +0100

    Update Readme

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
 # Binary Classification of Machine Failure
- Simple Deep Learning algorithm to predict if a machine will fail
+
+This repository contains a binary classification project focused on predicting machine failures using deep learning techniques. The project uses Keras with TensorFlow as a backend. The goal is to build a model that can effectively classify whether a machine is likely to fail based on provided data.
+
+## Dataset
+The project utilizes the dataset from a Kaggle competition. The dataset contains essential features for predicting machine failures. You can find the competition dataset at the following link:
+
+https://www.kaggle.com/competitions/playground-series-s3e17
+
+## Project Overview
+
+The primary components of this project are as follows:
+
+- **`code`**: This directory contains the main Python script, `dnn.py`, which executes the deep learning techniques for machine failure prediction. It also includes development notebooks (`dnn.ipynb`) for experimenting with new features and an EDA notebook (`eda.ipynb`) used for exploratory data analysis.
+
+- **`data`**: This directory holds the training and test datasets used for training and evaluating the model. The datasets are stored in separate directories, `train_set` and `test_set`, respectively.
+
+- **`HyperOptModelsHistory`**: This directory tracks the historical results of different models obtained through hyperparameter optimization using Keras Tuner. This information is useful for comparing model performance.
+
+- **`environment.yml` and `requirements.txt`**: These files specify the required dependencies to recreate the project environment using Conda or pip.
+
+## Getting Started
+
+To replicate the environment and run the project, follow these steps:
+
+1. Clone this repository to your local machine:
+
+   ```
+   git clone https://github.com/alessandroAbati/Binary-Classification-of-Machine-Failure
+   cd Binary-Classification-of-Machine-Failure
+   ```
+
+2. Set up the environment using Conda or pip:
+
+   - Using Conda:
+     ```
+     conda env create -f environment.yml
+     conda activate <environment_name>
+     ```
+
+   - Using pip:
+     ```
+     pip install -r requirements.txt
+     ```
+
+3. Prepare the datasets:
+   - Place your training dataset in the `data/train_set` directory.
+   - Place your test dataset in the `data/test_set` directory.
+
+   *Note: the dataset 
+
+4. Run the main script for model training and evaluation:
+
+   ```
+   python code/dnn.py
+   ```
+
+The script performs data preprocessing, feature engineering, hyperparameter tuning, model training, and evaluation. It also generates ROC curves and displays the Area Under the Curve (AUC) score.
+
+## Additional Information
+
+- The `create_features` function in the script performs feature engineering to improve the model's predictive performance.
+
+- The project uses a custom hyperparameter search strategy using Keras Tuner to find the best model configuration.
+
+- The model is trained on the training dataset and evaluated on the validation dataset, and the best model is selected for final evaluation.
+
+- The project handles class imbalance by applying class weights during model training.
+
+- The `dnn.ipynb` notebook provides a flexible environment for further development and experimentation with new features or techniques.
+
+## Results
+
+The script generates an AUC score as a measure of the model's performance. The ROC curve is plotted to visualize the trade-off between true positive rate and false positive rate. Additionally, the script generates predictions on the validation dataset.
+
+## Contributors
+
+- [Your Name](https://github.com/yourusername)
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Acknowledgments
+
+- Special thanks to the creators and maintainers of Keras, TensorFlow, and Keras Tuner for providing powerful tools for deep learning model development.
+
+Feel free to customize this README to provide more details specific to your project or add any additional sections that may be relevant.
--- code/dnn_prediction.py
+++ code/dnn_prediction.py
@@ -1,3 +1,6 @@
+############################################################
+#####     BINARY CLASSIFICATION OF MACHINE FAILURE     #####
+############################################################
 
 #import libraries
 import pandas as pd
@@ -8,6 +11,47 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.utils import class_weight
 import numpy as np
 import matplotlib.pyplot as plt
+import keras_tuner
+from keras.callbacks import EarlyStopping
+from sklearn.metrics import roc_curve, roc_auc_score
+
+#import data
+train = pd.read_csv("./data/train.csv")
+test = pd.read_csv("./data/test.csv")
+dataset = pd.concat([train,test])
+
+############################
+###  DATA PREPROCESSING  ###
+############################
+
+#handling duplicates
+train.drop_duplicates(subset=train.columns.difference(['id']),inplace=True)
+test.drop_duplicates(subset=test.columns.difference(['id']), inplace=True)
+
+#missing values
+train.dropna(inplace=True)
+test.dropna(inplace=True)
+
+#Searching for non-ordinal categorical features
+categorical_columns = train.select_dtypes(include=['object']).columns.values
+#Calculating unique values of categorical features
+for col in categorical_columns:
+    print(f" train {col}.unique = {len(train[col].unique())}, test {col}.unique = {len(test[col].unique())}") 
+#ONE-HOT ENCODING of Type column
+for df in [train, test]:
+    for value in df.Type.unique():
+        df[f'Type{value}'] = 0
+        df.loc[df.Type == f'{value}', f'Type{value}'] = 1
+    df.drop(columns=['Type'], inplace=True)
+#Frequency ENCODING of Product ID column (It is a way to utilize the frequency of the categories as labels)
+for df in [train, test]:
+    df['EncodedProductID'] = df.groupby(by=['Product ID'])['Product ID'].transform('count')
+    df.drop(columns=['Product ID'], inplace=True)
+
+
+#############################
+###  FEATURE ENGINEERING  ###
+#############################
 
 def create_features(df):
     
@@ -35,70 +79,113 @@ def create_features(df):
     
     return df
 
-#import data
-train = pd.read_csv("../data/train.csv")
-test = pd.read_csv("../data/test.csv")
-dataset = pd.concat([train,test])
-dataset.head()
-
-#DUPLICATES
-train.drop_duplicates(subset=train.columns.difference(['id']),inplace=True)
-test.drop_duplicates(subset=test.columns.difference(['id']), inplace=True)
-#MISSING VALUES
-train.dropna(inplace=True)
-test.dropna(inplace=True)
-#Searching for non-ordinal categorical features
-categorical_columns = train.select_dtypes(include=['object']).columns.values
-#Calculating unique values of categorical features
-for col in categorical_columns:
-    print(f" train {col}.unique = {len(train[col].unique())}, test {col}.unique = {len(test[col].unique())}") 
-#ONE-HOT ENCODING of Type column
-for df in [train, test]:
-    for value in df.Type.unique():
-        df[f'Type{value}'] = 0
-        df.loc[df.Type == f'{value}', f'Type{value}'] = 1
-    df.drop(columns=['Type'], inplace=True)
-#Frequency ENCODING of Product ID column (It is a way to utilize the frequency of the categories as labels)
-for df in [train, test]:
-    df['EncodedProductID'] = df.groupby(by=['Product ID'])['Product ID'].transform('count')
-    df.drop(columns=['Product ID'], inplace=True)
-
 train = create_features(train)
+test = create_features(test)
 
+
+#########################
+#####     MODEL     #####
+#########################
+
+class MyHyperModel(keras_tuner.HyperModel):
+    def build(self, hp):
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.InputLayer(input_shape=(n_features,)))
+        model.add(tf.keras.layers.Dense(units=hp.Int("layer1", min_value=8, max_value=512, step=16), activation='relu'))
+        if hp.Boolean("BatchNormalization1", default=True):
+            model.add(tf.keras.layers.BatchNormalization())
+        for i in range(hp.Int('number-of-hidden-layers', 1, 3, default=1)):
+            model.add(tf.keras.layers.Dense(units=hp.Int("hidden-layer"+str(i), min_value=32, max_value=512, step=32), activation='relu'))
+            if hp.Boolean("BatchNormalization"+str(i), default=True):
+                model.add(tf.keras.layers.BatchNormalization())
+            if i > 1:
+                if hp.Boolean("Dropout"+str(i-1), default=False):
+                    model.add(tf.keras.layers.Dropout(hp.Float('dropout-'+str(i-1), 0, 0.5, step=0.1, default=0.5)))
+        model.add(tf.keras.layers.Dense(units=hp.Int("final-layer", min_value=32, max_value=512, step=32), activation='relu'))
+        model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+
+        model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=False), optimizer=tf.keras.optimizers.Adam(learning_rate=hp.Float('learning_rate', 1e-4, 1e-2, sampling='log')), metrics=['AUC'])
+        model.summary()
+
+        return model
+    
+    def fit(self, hp, model, x, y, validation_data=None, **kwargs):
+        return model.fit(
+            x,
+            y,
+            batch_size = hp.Int("batch_size", min_value=8, max_value=512, step=8),
+            validation_data=validation_data,
+            **kwargs,
+        )
+    
 train_X = train.drop(columns=['id', 'Machine failure']).reset_index(drop=True)
 train_y = train['Machine failure'].reset_index(drop=True)
 n_features = len(train_X.columns)
+test_X = test.drop(columns=['id']).reset_index(drop=True)
 
 # StandardScaler
 sc = StandardScaler() # MinMaxScaler or StandardScaler
 train_X = sc.fit_transform(train_X)
+test_X = sc.fit_transform(test_X)
 
-model = tf.keras.Sequential()
-model.add(tf.keras.layers.InputLayer(input_shape=(n_features,)))
-model.add(tf.keras.layers.Dense(32, activation='relu'))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.Dense(64, activation='relu'))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.Dense(32, activation='relu'))
-model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
-
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['AUC'])
-model.summary()
+# Splitting train dataset into train and val
+train_X, val_X, train_y, val_y = train_test_split(train_X, train_y, test_size=0.33)
 
 #weighting the unbalanced target
-#class_weights = dict(enumerate(class_weight.compute_class_weight('balanced', np.unique(train_y), train_y)))
+class_weights = dict(enumerate(class_weight.compute_class_weight(class_weight='balanced',
+                                                 classes=np.unique(train_y),
+                                                 y=train_y)))
 
-from keras.callbacks import EarlyStopping
-early_stopping = EarlyStopping(monitor='val_loss', patience=100)
-print("\n")
-print(tf.config.list_physical_devices('GPU'))
-print("\n")
-#history = model.fit(train_X, train_y, batch_size=256, epochs=1000, class_weight=class_weights, callbacks=[early_stopping], validation_split=0.25)
-history = model.fit(train_X, train_y, batch_size=256, epochs=1000, callbacks=[early_stopping], validation_split=0.25)
+#initialization of keras tuner
+tuner = keras_tuner.BayesianOptimization(
+    hypermodel=MyHyperModel(),
+    objective=keras_tuner.Objective("val_auc", direction="max"),
+    max_trials=1,
+    num_initial_points = None,
+    overwrite=True,
+    directory="../hyperOptModelsHistory",
+    project_name="BinaryClassificationofMachineFailure",
+)
 
+# Searching the best hyperparameters
+tuner.search(train_X, train_y, epochs=1, validation_data=(val_X, val_y), class_weight=class_weights, callbacks=[tf.keras.callbacks.EarlyStopping(patience=30)])
+
+# Get the top 2 models.
+models = tuner.get_best_models(num_models=2)
+best_model = models[0]
+# Build the model.
+# Needed for `Sequential` without specified `input_shape`.
+best_model.build(input_shape=(None, 28, 28))
+best_model.summary()
+
+#Retrain the best model with the best hps
+hypermodel = MyHyperModel()
+best_hp = tuner.get_best_hyperparameters()[0]
+model = hypermodel.build(best_hp)
+# Fit with the entire dataset.
+train_X = np.concatenate((train_X, val_X))
+train_y = np.concatenate((train_y, val_y))
+history = hypermodel.fit(best_hp, model, train_X, train_y, epochs=1, class_weight=class_weights, callbacks=[tf.keras.callbacks.EarlyStopping(patience=30)], validation_split=0.2)
+
+# Plotting loss
 plt.plot(history.history['loss'], label='train')
 plt.plot(history.history['val_loss'], label='val')
 plt.legend()
 plt.show()
+
+# Generate Prediction
+y_pred = model.predict(val_X)
+fpr, tpr, _ = roc_curve(val_y,  y_pred)
+auc = roc_auc_score(val_y, y_pred)
+plt.plot(fpr,tpr,label="auc="+str(auc))
+plt.legend(loc=4)
+plt.show()
+
+print(f"AUC: {auc}")
+print(f"\nPrediction:\n {y_pred}")
+
+#create output
+output = pd.DataFrame({
+    "Machine failure" : np.squeeze(y_pred)
+})
+print(output[output['Machine failure']>0.5].sort_values(by=['Machine failure'], ascending=False))
